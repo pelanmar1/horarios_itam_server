0008ee55c665e79b5983073f535277ee02e89ae8
Pedro Lanzagorta <t-pelanz@Temps-MacBook-Pro.local>
2017-08-25 16:02:22 -0700

    Algun cambio curioso.

--- server.js
+++ server.js
@@ -18,8 +18,7 @@ app.use(function(req, res, next) {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,contenttype");
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); 
-    res.setHeader('Access-Control-Allow-Credentials', true); 
-
+    res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 });
 app.use(express.static(path.join(__dirname, 'public')));
