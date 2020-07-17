b92431374bfa516b6451eb9db3c2c977a98d5283
Pedro Lanzagorta <pelanmar1@gmail.com>
2020-07-16 19:31:12 -0700

    Update server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 
 // HTTP GETs
 app.get('/',function (req,res) {
-    res.send('Verano 2020');
+    res.send('Otoño 2020');
 });
 app.get('/loadSchedules',function (req,res) {
     var courseName = req.query.cName;
